57b4d6c507ead7bdd8a6ef7cb35b3020845dbb27
Will Jones <william.jones@audacia.co.uk>
2019-03-11 16:27:36 +0000

    Added column width as property of column and refactored code to fit.

--- src/Audacia.Spreadsheets/Table.cs
+++ src/Audacia.Spreadsheets/Table.cs
@@ -12,17 +12,17 @@ namespace Audacia.Spreadsheets
         public Table() { }
 
         public Table(bool includeHeaders) => IncludeHeaders = includeHeaders;
-        
+
         public string StartingCellRef { get; set; } = "A1";
-        
+
         public TableHeaderStyle HeaderStyle { get; set; }
 
         public bool IncludeHeaders { get; set; }
-        
+
         public List<TableColumn> Columns { get; } = new List<TableColumn>();
 
         public List<TableRow> Rows { get; } = new List<TableRow>();
-        
+
         public void Write(SharedDataTable sharedData, OpenXmlWriter writer)
         {
             var rowReference = new CellReference(StartingCellRef);
@@ -55,7 +55,7 @@ namespace Audacia.Spreadsheets
                 {
                     var isFirstColumn = column == Columns.ElementAt(0);
                     var isLastColumn = column == Columns.ElementAt(Columns.Count - 1);
-                    column.Write(HeaderStyle, headerCellRef, isFirstColumn,isLastColumn, sharedData, writer);
+                    column.Write(HeaderStyle, headerCellRef, isFirstColumn, isLastColumn, sharedData, writer);
                     headerCellRef.NextColumn();
                 }
 
@@ -71,75 +71,65 @@ namespace Audacia.Spreadsheets
             }
         }
 
-        
-        public static Dictionary<int, int> GetMaxCharacterWidth(Table model)
+
+        public static int GetMaxCharacterWidth(Table table, int columnIndex)
         {
-            //iterate over all cells getting a max char value for each column
-            var maxColWidth = new Dictionary<int, int>();
+            var column = table.Columns[columnIndex];
 
-            // Create Cells for Data
-            var columnHeaderWithData = model.Rows.ToList();
-
-            // Create Cells for Headers
-            var rowCells = model.Columns.Select(c => new TableCell(c.Name));
-            var row = TableRow.FromCells(rowCells, 0);
-            
-            columnHeaderWithData.Add(row);
-            
-            // Create Cells for Rollups
-            if (model.Columns.Any(c => c.DisplaySubtotal))
+            if (column.Width.HasValue)
             {
-                var rollupCells = model.Columns
-                    .Select((col, index) => new {col, index})
-                    .Where(t => t.col.DisplaySubtotal)
-                    .Select(t => model.Rows
-                        .Where(r => r.Cells.Count > t.index)
+                return column.Width.Value;
+            }
+
+            var cells = table.Rows.Select(r => r.Cells.Count > columnIndex ? r.Cells[columnIndex] : null).Where(c => c != null).ToList();
+
+            if (table.IncludeHeaders)
+            {
+                cells.Add(new TableCell(column.Name));
+            }
+
+            // Create Cells for Rollups
+            if (column.DisplaySubtotal)
+            {
+                var total = table.Rows
+                        .Where(r => r.Cells.Count > columnIndex)
                         .Select(r =>
                         {
-                            var value = r.Cells.ElementAt(t.index).Value;
+                            var value = r.Cells.ElementAt(columnIndex).Value;
                             var isNumeric = value.GetType().IsNumeric();
                             return isNumeric ? Convert.ToDecimal(value) : 0;
                         })
                         .DefaultIfEmpty(0)
-                        .Sum(v => v))
-                    .Select(value => new TableCell
-                    {
-                        // Format as currency because the number value alone just isn't long enough
-                        Value = $"{value:C}"
-                    });
-                
-                var rollupRow = TableRow.FromCells(rollupCells, 0);
-                columnHeaderWithData.Add(rollupRow);
+                        .Sum(v => v);
+                cells.Add(new TableCell
+                {
+                    // Format as currency because the number value alone just isn't long enough
+                    Value = $"{total:C}"
+                });
             }
 
-            // Find the max cell width of each column
-            foreach (var r in columnHeaderWithData)
+            // Find the max cell width of each column           
+            var current = 0;
+
+            //using cell index as my column
+            for (var i = 0; i < cells.Count; i++)
             {
-                var cells = r.Cells.ToArray();
+                var cell = cells[i];
+                var cellValue = cell.Value?.ToString() ?? string.Empty;
+                var cellTextLength = cellValue.Length;
 
-                //using cell index as my column
-                for (var i = 0; i < cells.Length; i++)
+                if (cellTextLength > current)
                 {
-                    var cell = cells[i];
-                    var cellValue = cell.Value?.ToString() ?? string.Empty;
-                    var cellTextLength = cellValue.Length;
-
-                    if (maxColWidth.ContainsKey(i))
-                    {
-                        var current = maxColWidth[i];
-                        if (cellTextLength > current)
-                        {
-                            maxColWidth[i] = cellTextLength;
-                        }
-                    }
-                    else
-                    {
-                        maxColWidth.Add(i, cellTextLength);
-                    }
+                    current = cellTextLength;
                 }
             }
 
-            return maxColWidth;
+            if (current > 75)
+            {
+                current = 75;
+            }
+
+            return current;
         }
     }
 }
